1d1a05353afc7068abfe222e6b5579369ac9f5a1
Родион Шукало <ban.zhmyh@yandex.ru>
2023-11-06 18:41:54 +0300

    code according to PEP8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,9 +108,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static_backend/'
-# Укажите директорию, куда бэкенд-приложение должно сложить статику.
-STATIC_ROOT = BASE_DIR / 'static_backend'
+STATIC_URL = '/static/'
+# Указываем корневую директорию для сборки статических файлов;
+# в контейнере это будет /app/collected_static
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
